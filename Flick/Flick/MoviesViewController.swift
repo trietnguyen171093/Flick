28489ab8c0166e4e89b8913bbd9fa3fb9758ec8e
Triet Nguyen <triet@fossil.com>
2017-06-18 23:51:37 +0700

    - Add network UIView

--- Flick/Flick/MoviesViewController.swift
+++ Flick/Flick/MoviesViewController.swift
@@ -18,6 +18,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
   
     var movies = [NSDictionary]()
     let refreshControl = UIRefreshControl()
+    var  errView: UIView?
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,41 +33,85 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
       refreshControl.addTarget(self, action: #selector(MoviesViewController.fetchList), for: UIControlEvents.valueChanged)
       tableView.addSubview(refreshControl) // not required when using UITableViewController
     }
-    func fetchList() {
+      func fetchList() {
       // Code to refresh table view
       refreshControl.beginRefreshing()
       networkFetch()
       refreshControl.endRefreshing()
     }
-
     func networkFetch()
     {
-      let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-      let url = URL(string: "http://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-      let request = URLRequest(
-        url: url!,
-        cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
-        timeoutInterval: 100)
-      let session = URLSession(
-        configuration: URLSessionConfiguration.default,
-        delegate: nil,
-        delegateQueue: OperationQueue.main
-      )
-      let task: URLSessionDataTask =
-        session.dataTask(with: request,
-                         completionHandler: { (dataOrNil, response, error) in
-                          if let data = dataOrNil {
-                            if let responseDictionary = try! JSONSerialization.jsonObject(
-                              with: data, options:[]) as? NSDictionary {
-                              
-                              self.movies = responseDictionary["results"] as! [NSDictionary]
-                              print("response: \(self.movies)")
-                              self.tableView.reloadData()
+      if isInternetAvailable(){
+        // Hide the UIView if network is connected
+        errView?.isHidden = true
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "http://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(
+          url: url!,
+          cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+          timeoutInterval: 100)
+        let session = URLSession(
+          configuration: URLSessionConfiguration.default,
+          delegate: nil,
+          delegateQueue: OperationQueue.main
+        )
+        let task: URLSessionDataTask =
+          session.dataTask(with: request,
+                           completionHandler: { (dataOrNil, response, error) in
+                            if let data = dataOrNil {
+                              if let responseDictionary = try! JSONSerialization.jsonObject(
+                                with: data, options:[]) as? NSDictionary {
+                                
+                                self.movies = responseDictionary["results"] as! [NSDictionary]
+                                print("response: \(self.movies)")
+                                self.tableView.reloadData()
+                              }
                             }
-                          }
-        })
-      task.resume()
+          })
+        task.resume()
+        
+      }
+      else{
+        // debug print
+        print("Network error")
+        let errorViewFrame : CGRect = CGRect(x: 0, y: 500, width: 360, height: 40)
+        errView = UIView(frame: errorViewFrame)
+        errView?.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        let errLabel = UILabel(frame: CGRect(x: 0, y: 0, width: (errView?.frame.width)!, height: (errView?.frame.height)!))
+        errLabel.textColor = UIColor.white
+        errLabel.textAlignment = .center
+        errLabel.text = "Network Error!"
+        errLabel.isHighlighted = true
+        errView?.addSubview(errLabel)
+        
+        self.view.addSubview(errView!)
+        errView?.isHidden = false
+      }
+      
     }
+    // This func check network avaiable or not
+    func isInternetAvailable() -> Bool
+    {
+      var zeroAddress = sockaddr_in()
+      zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+      zeroAddress.sin_family = sa_family_t(AF_INET)
+      
+      let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+          SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        }
+      }
+      
+      var flags = SCNetworkReachabilityFlags()
+      if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        return false
+      }
+      let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+      let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+      return (isReachable && !needsConnection)
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
       return movies.count
     }
